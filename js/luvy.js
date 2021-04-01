da82b4b84091c72adb94f4bf3036f46374a45d58
luvymhq <luvymhq@163.com>
2021-04-01 22:50:40 +0800

    Site updated: 2021-04-01 22:50:39

--- js/luvy.js
+++ js/luvy.js
@@ -1,3 +1,9 @@
+/*添加图片top*/
+var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+/*添加到返回顶部按钮下*/
+document.getElementById("go-up").innerHTML += top_up;
+
+
 // 气泡
 function qipao() {
     $('#page-header').circleMagic({
@@ -45,3 +51,7 @@ function qipao() {
 
 // 调用气泡方法
 qipao();
+
+$(document).ready(function(e) {
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lete乐特');
+});
